0c166339dac1f7a0045a1fe1f60109a020e30e1e
Stan Vlad <vstan02@protonmail.com>
2021-02-10 20:35:07 +0200

    Make Panel  responsive

--- src/components/base/BasePanel.tsx
+++ src/components/base/BasePanel.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { LogoutButton } from '../buttons';
+import { LogoutButton, BaseButton } from '../buttons';
 
-const PanelRoot = styled.aside`
+interface PanelRootProps {
+	open: boolean;
+}
+
+const PanelRoot = styled.aside<PanelRootProps>`
 	width: 420px;
 	height: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	background-color: ${ ({ theme }) => theme.color.BASE_ALPHA };
+	z-index: 5;
+	transition: 1s left;
+	
+	@media (max-width: 1024px) {
+		position: absolute;
+		left: ${ (props: any) => props.open ? '0' : '-420px' };
+	}
+`;
+
+const PanelClose = styled(BaseButton)`
+	display: none;
+	
+	@media (max-width: 1024px) {
+		display: block;
+		width: 50px;
+		height: 50px;
+		background-color: inherit;
+		position: absolute;
+		left: 420px;
+		top: 10px;
+		border-radius: 0 3px 3px 0;
+	}
 `;
 
 const PanelHead = styled.div`
@@ -29,13 +55,21 @@ const PanelContent = styled.div`
 	overflow: hidden;
 `;
 
-export const BasePanel: React.FC = props => (
-	<PanelRoot>
-		<PanelHead>
-			<LogoutButton>Logout</LogoutButton>
-		</PanelHead>
-		<PanelContent>
-			{ props.children }
-		</PanelContent>
-	</PanelRoot>
-);
+export const BasePanel: React.FC = props => {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<PanelRoot open={ open }>
+			<PanelClose
+				children='x'
+				onClick={ () => setOpen(!open) }
+			/>
+			<PanelHead>
+				<LogoutButton>Logout</LogoutButton>
+			</PanelHead>
+			<PanelContent>
+				{ props.children }
+			</PanelContent>
+		</PanelRoot>
+	);
+};
