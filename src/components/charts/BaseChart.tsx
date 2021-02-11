fefa0582fd20c40ed5992ba1a82b65e3fdd57e05
Stan Vlad <vstan02@protonmail.com>
2021-02-11 22:21:25 +0200

    Update BaseChart component

--- src/components/charts/BaseChart.tsx
+++ src/components/charts/BaseChart.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
 import { AreaChart, Area, Tooltip, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 interface BaseChartProps {
@@ -7,25 +8,34 @@ interface BaseChartProps {
 	data: Array<object>;
 }
 
-export const BaseChart: React.FC<BaseChartProps> = props => (
-	<ResponsiveContainer>
-		<AreaChart data={ props.data }>
-			<XAxis dataKey={ props.keys } stroke="#FFFFFF" />
-			<YAxis
-				dataKey={ props.values }
-				stroke="#FFFFFF"
-				domain={[0, (max: number) => (max * 1.5)]}
-				allowDataOverflow
-			/>
-			<Tooltip />
-			<CartesianGrid stroke="#172331" strokeDasharray="2 2" />
-			<Area
-				type="monotone"
-				dataKey={ props.values }
-				stroke="#7bcbf0"
-				fillOpacity={ 0.45 }
-				fill="#7bcbf0"
-			/>
-		</AreaChart>
-	</ResponsiveContainer>
-);
+export const BaseChart: React.FC<BaseChartProps> = props => {
+	const theme = useContext(ThemeContext);
+	return (
+		<ResponsiveContainer>
+			<AreaChart data={ props.data }>
+				<Tooltip/>
+				<XAxis
+					dataKey={ props.keys }
+					stroke={ theme.color.CONTRAST_ALPHA }
+				/>
+				<YAxis
+					dataKey={ props.values }
+					stroke={ theme.color.CONTRAST_ALPHA }
+					domain={ [0, (max: number) => (max * 1.5)] }
+					allowDataOverflow
+				/>
+				<CartesianGrid
+					stroke={ theme.color.BASE_ALPHA }
+					strokeDasharray="2 2"
+				/>
+				<Area
+					type="monotone"
+					dataKey={ props.values }
+					stroke={ theme.color.PRIMARY_BETA }
+					fillOpacity={ 0.45 }
+					fill={ theme.color.PRIMARY_BETA }
+				/>
+			</AreaChart>
+		</ResponsiveContainer>
+	);
+};
