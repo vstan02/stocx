c52d4186d5eead0bb51960b5663d2ed49142bda6
Stan Vlad <vstan02@protonmail.com>
2021-03-29 18:16:52 +0300

    Sync

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-export const BASE_API = 'https://stocx-api.herokuapp.com';
 export const FINANCE_API = 'https://twelve-data1.p.rapidapi.com';
-
 export const FINANCE_KEY = 'b98fda632fmsh31ab6cae2fcbc75p14d55fjsn6f095670f203';
 export const FINANCE_HOST = 'twelve-data1.p.rapidapi.com';
--- src/hooks/http.hook.ts
+++ src/hooks/http.hook.ts
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 import axios from 'axios';
 
-import { BASE_API, FINANCE_API, FINANCE_HOST, FINANCE_KEY } from '../config';
+import { FINANCE_API, FINANCE_HOST, FINANCE_KEY } from '../config';
 
 export enum HttpMethod {
 	GET = 'GET',
@@ -14,29 +14,6 @@ interface HttpHook {
 	abort(): void;
 	clearError(): void;
 }
-
-export const useBaseApi = (): HttpHook => {
-	const source = axios.CancelToken.source();
-	const [error, setError] = useState('');
-
-	const request = useCallback(async (uri, method = HttpMethod.GET, data = {}) => {
-		try {
-			const url = BASE_API + uri;
-			const cancelToken = source.token;
-			const  response = await axios({ url, method, data, cancelToken });
-
-			if (response.data.status >= 300)
-				return setError(response.data.details);
-			return response.data;
-		} catch (e) {
-			setError(e.message);
-		}
-	}, []);
-
-	const abort = () => source.cancel();
-	const clearError = () => setError('');
-	return { error, request, abort, clearError };
-};
 
 export const useFinanceApi = (): HttpHook => {
 	const source = axios.CancelToken.source();
